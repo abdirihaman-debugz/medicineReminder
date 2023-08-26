632835f2d3355d9311ed4f7c2bca557bc187215d
a <aabdirihaman@gmail.com>
2023-08-26 09:56:34 -0700

    fixed timing and added text component

--- reminder.py
+++ reminder.py
@@ -1,33 +1,30 @@
 import datetime
 import smtplib
-from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import os
 
 old_time = datetime.datetime.now()
-new_time = old_time + datetime.timedelta(hours=6, minutes=0)
-message = "TIME TO TAKE YOUR MEDICINE NOW " + old_time.strftime("%I:%M %p" ) + " \n" + "Next time you need to take your medicine: " + new_time.strftime("%I:%M %p")
+new_time = old_time + datetime.timedelta(hours=4, minutes=0)
+message = "Medicine Reminder\nTIME TO TAKE YOUR MEDICINE NOW " + old_time.strftime("%I:%M %p" ) + " \n" + "Next time you need to take your medicine: " + new_time.strftime("%I:%M %p")
 print(message)
 
 
 
 
 # Email configuration
-USER_EMAIL = os.environ.get("USER_EMAIL")
-USER_PASSWORD = os.environ.get("USER_PASSWORD")
+USER_EMAIL = "cronbaby2023@gmail.com"
+USER_PASSWORD = "yzydscwcfjtwkveo"
+
 sender_email = USER_EMAIL
-receiver_email = USER_EMAIL  # Use the same email address for sender and receiver
-password = USER_PASSWORD
-subject = "Medicine Reminder"
+sender_password  = USER_PASSWORD
+
+recipient_phone = "7142669806"  # Recipient's phone number
+carrier_gateway = "7142669806@tmomail.net"  # T-Mobile's email-to-SMS gateway
 
 # Create a MIMEText object for the email content
-email_body = MIMEMultipart()
-email_body.attach(MIMEText(message, "plain"))
-
-# Set the email headers
-email_body["From"] = sender_email
-email_body["To"] = receiver_email
-email_body["Subject"] = subject
+msg = MIMEText(message)
+msg["From"] = sender_email
+msg["To"] = carrier_gateway
 
 # Connect to the SMTP server
 smtp_server = "smtp.gmail.com"  # Example for Gmail
@@ -39,14 +36,16 @@ try:
     server.starttls()  # Upgrade the connection to secure (TLS)
 
     # Log in to your email account
-    server.login(sender_email, password)
+    server.login(sender_email, sender_password)
 
     # Send the email
-    server.sendmail(sender_email, receiver_email, email_body.as_string())
-
-    # Close the SMTP server connection
+    server.sendmail(sender_email, carrier_gateway, msg.as_string())
     server.quit()
-    print("Email sent successfully!")
+
+    print("Text message sent successfully")
 
 except Exception as e:
     print(f"An error occurred: {str(e)}")
+
+
+
